ab2b1901cc0323b517e4fd8a9d7d30c4036be182
prathapsankaran <prathap@ittblazers.com>
2019-03-27 14:59:19 +0530

    LSP Security Module composer File

--- module_blacklist/src/Exception/ModuleInstallationBlocked.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\module_blacklist\Exception;
-
-/**
- * Exception class to throw when a blacklisted module is being installed.
- */
-class ModuleInstallationBlocked extends \Exception {}
